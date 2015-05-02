a4e38ffbd0e0d90204c29c5b6218d3d2922f47c2
peterbolson <peterbolson@gmail.com>
2015-05-02 16:00:49 -0500

    Changed welcome text

--- hillary/views.py
+++ hillary/views.py
@@ -5,6 +5,6 @@ from .word_cloud import WordCloudScraper
 
 @app.route('/')
 def index():
-    welcome_message = 'Hello World! You are looking might fine today.'
+    welcome_message = 'NO.'
     return render_template('index.html', welcome_message=welcome_message)
 
