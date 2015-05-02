ac174c2e1cb4a14f260aa33ec188490046846391
Phil Ngo <ngo.phil@gmail.com>
2015-05-02 16:18:13 -0500

    add draft of word cloud

--- hillary/views.py
+++ hillary/views.py
@@ -1,10 +1,14 @@
 from flask import render_template
 from . import app
 
-from .word_cloud import WordCloudScraper
+from word_cloud import WordCloudScraper
+import json
 
 @app.route('/')
 def index():
+    wcs = WordCloudScraper()
+    words = wcs.word_counts()
     welcome_message = 'Hello World! You are looking might fine today.'
-    return render_template('index.html', welcome_message=welcome_message)
-
+    return render_template('index.html',
+            welcome_message=welcome_message,
+            words=json.dumps(words))
